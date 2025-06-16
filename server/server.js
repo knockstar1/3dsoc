4461697373258cd05d7a5076f90a282f0790df2f
Knock Star <knockstaranimations@gmail.com>
2025-06-15 20:33:27 -0400

    Fix static files root path

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ dotenv.config();
 const app = express();
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '..')));
 
 // Middleware
 app.use(express.json());
